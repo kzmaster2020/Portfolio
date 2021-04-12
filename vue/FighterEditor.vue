214e207e755e2d3e59fd1f4cd436093a0c36bbde
Kellen Zanders <65644862+kzmaster2020@users.noreply.github.com>
2021-04-12 11:57:38 -0500

    Add files via upload

---
+++ vue/FighterEditor.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+
+        <button class="btn btn-sm btn-outline-danger" @click='deleteFighter'>Delete</button>
+    </div>
+    
+</template>
+
+<script>
+import { apiService } from '../common/api.service';
+export default {
+    name: 'FighterEditor',
+    props: {
+        first_name: {
+            type: String, 
+            required: true
+        }
+    }, 
+    methods: {
+        async deleteFighter() {
+            let endpoint = `/api/fightmanager/${this.first_name}/`;
+            try {
+                await apiService(endpoint, 'DELETE');
+                this.$router.push('/')
+            }
+            catch(err) {
+                console.log(err);
+            }
+        }
+    }
+}
+</script>
